c6f28cc03d79b283fd7e6c5a1205d540b0e46b53
Daniel Pinzon <pinzondiazdaniel@gmail.com>
2021-12-01 17:31:18 -0500

    Agregando el codigo de ahorcado

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,5 +1,31 @@
-const palabras = ["gato" , "murcielago" , "rinoceronte"]
+let words = ["violeta", "gato", "perro"];
+let word = words[Math.floor(Math.random()*words.length)];
 
-const palabra = palabras[Math.floor(Math.random()*palabras.length)]
 
-console.log(palabra)
+const wordSplit = word.split("");
+
+let answerArray = [];
+for (let i = 0; i < word.length; i++) {
+  answerArray[i] = "_";
+}
+
+let letras = word.length;
+
+while (letras > 0) {
+      (answerArray.join(" "));
+      let attempt = prompt("Adivina una letra");
+      if (attempt === null) {
+        break;
+      } else if (attempt.length !== 1) {
+        alert("Por favor escribe una sola letra");
+      } else {
+        for (let x = 0; x < word.length; x++) {
+        if (word[x] === attempt) {
+        answerArray[x] = attempt;
+        letras--;
+          }
+        }
+      }
+    }
+
+alert(`¡Adivinaste! la palabra era ${word}`)
