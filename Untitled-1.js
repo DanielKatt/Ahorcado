17150bcac5a4c420aedf33bc52b5c9ac39c94cd0
Daniel Pinzon <pinzondiazdaniel@gmail.com>
2021-12-02 23:22:12 -0500

    Despues de tanto sufriemiento.

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,31 +1,37 @@
-let words = ["violeta", "gato", "perro"];
-let word = words[Math.floor(Math.random()*words.length)];
-
-
-const wordSplit = word.split("");
-
-let answerArray = [];
-for (let i = 0; i < word.length; i++) {
-  answerArray[i] = "_";
-}
-
-let letras = word.length;
-
-while (letras > 0) {
-      (answerArray.join(" "));
-      let attempt = prompt("Adivina una letra");
-      if (attempt === null) {
-        break;
-      } else if (attempt.length !== 1) {
-        alert("Por favor escribe una sola letra");
-      } else {
-        for (let x = 0; x < word.length; x++) {
-        if (word[x] === attempt) {
-        answerArray[x] = attempt;
-        letras--;
-          }
+// Variables
+const palabraJuego = "teclado";
+let palabraTamaño = palabraJuego.length;
+let letraEvaluar   = "";
+//let erroresMaximo = palabraJuego.length;
+const erroresMaximo = 6;
+ 
+let intentos = 0;
+let errores = 0;
+  // DOM ( elementos de HTML )
+ 
+  let input = document.getElementById("input-letra");
+  let boton = document.getElementById("evaluarletra");
+ 
+  jugada()
+ 
+function jugada() {
+  boton.addEventListener("click",()=>{
+    if (errores < erroresMaximo) {
+      letraEvaluar = input.value;
+      if ( letraEvaluar.length == 1 ) {
+     
+        if ( palabraJuego.includes(letraEvaluar) ) {
+          alert("si tiene la letra")
+        }else{
+          errores ++;
+          alert("no la tiene "+ errores)
         }
+        intentos ++;
+      }else{
+        alert("Porfa escribe solo una letra jeje");
       }
+    }else{
+      alert("je murio")
     }
-
-alert(`¡Adivinaste! la palabra era ${word}`)
+  });  
+}
